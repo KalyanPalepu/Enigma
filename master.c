1de460c6868fe54ddbd49ec59c3e0636821f5f7a
KingSting <KingSting@users.noreply.github.com>
2016-02-16 17:09:17 -0500

    Done

--- master.c
+++ master.c
@@ -1,42 +1,45 @@
 #include <Wire.h>
 
 //machine variables
-int lamp[] = {10,9,8,7,6,5,4,3,2};
-int lanode[] = {11,12,13};
-char firstRow[] = {'Q', 'U', 'E', 'R', 'T', 'Z', 'U', 'I', 'O'};
-char secondRow[] = {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K'};
-char thirdRow[] = {'P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L'};
-/*boolean lampvals[9][9] =   { { 0,1,1,1,1,1,1,1,1 },  // = Q or A or P
-                             { 1,0,1,1,1,1,1,1,1 },  // = W or S or Y
-                             { 1,1,0,1,1,1,1,1,1 },  // = E or D or X
-                             { 1,1,1,0,1,1,1,1,1 },  // = R or F or C
-                             { 1,1,1,1,0,1,1,1,1 },  // = T or G or V
-                             { 1,1,1,1,1,0,1,1,1 },  // = Z or H or B
-                             { 1,1,1,1,1,1,0,1,1 },  // = U or J or N
-                             { 1,1,1,1,1,1,1,0,1 },  // = I or K or M
-                             { 1,1,1,1,1,1,1,1,0 }   // = O or      L
-                            };*/
-                            
+int lamp[] = {
+  10,9,8,7,6,5,4,3,2};
+int lanode[] = {
+  11,12,13};
+
+char firstRow[] = {
+  'P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L'};
+char secondRow[] = {
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K'};
+char thirdRow[] = {
+  'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O'};
+
 char lastPressed = '?';
 
+boolean stepThirdNextTurn = false;
 
 
 //encoding variables
-char rotorOne[] = {'L', 'P', 'G', 'S', 'Z', 'M', 'H', 'A', 'E', 'O', 'Q', 'K', 'V', 'X', 'R', 'F', 'Y', 'B', 'U', 'T', 'N', 'I', 'C', 'J', 'D', 'W'};
-char rotorTwo[] = {'S', 'L', 'V', 'G', 'B', 'T', 'F', 'X', 'J', 'Q', 'O', 'H', 'E', 'W', 'I', 'R', 'Z', 'Y', 'A', 'M', 'K', 'P', 'C', 'N', 'D', 'U'};
-char rotorThree[] = {'C', 'J', 'G', 'D', 'P', 'S', 'H', 'K', 'T', 'U', 'R', 'A', 'W', 'Z', 'X', 'F', 'M', 'Y', 'N', 'Q', 'O', 'B', 'V', 'L', 'I', 'E'};
-char reflector[] = {'I', 'M', 'E', 'T', 'C', 'G', 'F', 'R', 'A', 'Y', 'S', 'Q', 'B', 'Z', 'X', 'W', 'L', 'H', 'K', 'D', 'V', 'U', 'P', 'O', 'J', 'N'};
+char rotorOne[] = {
+  'L', 'P', 'G', 'S', 'Z', 'M', 'H', 'A', 'E', 'O', 'Q', 'K', 'V', 'X', 'R', 'F', 'Y', 'B', 'U', 'T', 'N', 'I', 'C', 'J', 'D', 'W'};
+char rotorTwo[] = {
+  'S', 'L', 'V', 'G', 'B', 'T', 'F', 'X', 'J', 'Q', 'O', 'H', 'E', 'W', 'I', 'R', 'Z', 'Y', 'A', 'M', 'K', 'P', 'C', 'N', 'D', 'U'};
+char rotorThree[] = {
+  'C', 'J', 'G', 'D', 'P', 'S', 'H', 'K', 'T', 'U', 'R', 'A', 'W', 'Z', 'X', 'F', 'M', 'Y', 'N', 'Q', 'O', 'B', 'V', 'L', 'I', 'E'};
+char reflector[] = {
+  'I', 'M', 'E', 'T', 'C', 'G', 'F', 'R', 'A', 'Y', 'S', 'Q', 'B', 'Z', 'X', 'W', 'L', 'H', 'K', 'D', 'V', 'U', 'P', 'O', 'J', 'N'};
 
-char entryWheel[] = {'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L'};
+char entryWheel[] = {
+  'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L'};
 
-char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphabet[] = {
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 int ringOne = 1;
 int ringTwo = 1;
 int ringThree = 1;
 
 char rotorOnePlace = 'A';
-char rotorTwoPlace = 'A';
+char rotorTwoPlace = 'D';
 char rotorThreePlace = 'A';
 
 char turnoverOne = 'Y';
@@ -46,6 +49,7 @@ char turnoverThree = 'N';
 //main functions
 void setup() {
   Serial.begin(9600);
+
   Wire.begin();
   for(int i = 1; i < ringOne; i++){
     ringTurnRotor(rotorOne);
@@ -64,22 +68,26 @@ void setup() {
 
 void loop() {
   char pressed = readKey();
-  if(pressed != '?' && pressed != lastPressed){
-    Serial.println(pressed);
-
-    incrementRotors();
+  if(pressed == '?'){
+    lampOff();
+  }
+  else if(pressed != lastPressed){
     writeLamp(rotorEncodeAll(pressed));
+    incrementRotors();
 
     Wire.beginTransmission(8);
     Wire.write(rotorOnePlace);
     Wire.write(rotorTwoPlace);
     Wire.write(rotorThreePlace);
   }
+  lastPressed = pressed;
+  Serial.println(pressed);
+  delay(20);
 }
 
 //util functions
-boolean arrayContains(char array[], char element){
-  for(int i = 0; i < (sizeof(array) / sizeof(char)); i++){
+boolean arrayContains(char array[], char element, int length){
+  for(int i = 0; i < length; i++){
     if(array[i] == element){
       return true;
     }
@@ -90,45 +98,53 @@ boolean arrayContains(char array[], char element){
 //machine functions
 
 void writeLamp(char key){
-  if(arrayContains(firstRow, key)){
+  if(arrayContains(firstRow, key, 9)){
     digitalWrite(lanode[0], 1);
     digitalWrite(lanode[1], 0);
     digitalWrite(lanode[2], 0);
-    writeRow(firstRow, key);
+    writeRow(firstRow, key, 9);
   }
 
-  if(arrayContains(secondRow, key)){
+  if(arrayContains(secondRow, key, 8)){
     digitalWrite(lanode[0], 0);
     digitalWrite(lanode[1], 1);
     digitalWrite(lanode[2], 0);
-    writeRow(secondRow, key);
+    writeRow(secondRow, key, 8);
   }
 
-  if(arrayContains(thirdRow, key)){
+  if(arrayContains(thirdRow, key, 9)){
     digitalWrite(lanode[0], 0);
     digitalWrite(lanode[1], 0);
     digitalWrite(lanode[2], 1);
-    writeRow(thirdRow, key);
+    writeRow(thirdRow, key, 9);
   }
   else{
-    Serial.println(key);
+    Serial.println("Error: Invalid Key");
   }
 }
 
-void writeRow(char row[], char key){
+void writeRow(char row[], char key, int length){
   digitalWrite(lamp[8], 1);
-  for(int i = 0; i < (sizeof(row) / sizeof(char)); i++){
+  for(int i = 0; i < (length); i++){
     if(row[i] == key){
       digitalWrite(lamp[i], 0);
-    } else{
+    } 
+    else{
       digitalWrite(lamp[i], 1);
     }
   }
 }
 
+void lampOff(){
+  digitalWrite(lanode[0], 0);
+  digitalWrite(lanode[1], 0);
+  digitalWrite(lanode[2], 0); 
+}
+
 char readKey() {
   int reading = analogRead(A1);
-  if (reading >= 80 && reading <= 104) {}
+  if (reading >= 80 && reading <= 104) {
+  }
   else if (reading <= 54) return 'P';
   else if (reading <= 108) return 'L';
   else if (reading <= 118) return 'M';
@@ -140,7 +156,8 @@ char readKey() {
   else if (reading <= 355) return 'Y';
 
   reading = analogRead(A2);
-  if (reading >= 90 && reading <= 115) {}
+  if (reading >= 90 && reading <= 115) {
+  }
   else if (reading <= 60) return 'A';
   else if (reading <= 120) return 'K';
   else if (reading <= 133) return 'J';
@@ -151,7 +168,8 @@ char readKey() {
   else if (reading <= 400) return 'S';
 
   reading = analogRead(A3);
-  if (reading >= 80 && reading <= 103) {}
+  if (reading >= 80 && reading <= 103) {
+  }
   else if (reading <= 54) return 'Q';
   else if (reading <= 108) return 'O';
   else if (reading <= 118) return 'I';
@@ -168,46 +186,49 @@ char readKey() {
 //encoding functions
 char rotorEncode(char standard[26], char rotor[26], char key) {
   for(int i = 0; i < 26; i++){
-    if(standard[i] = key){
+    if(standard[i] == key){
       return rotor[i];
     }
   }
-  return '!';
+  return '?';
 }
 
 char rotorEncodeAll(char key){
   return rotorEncode(alphabet, entryWheel,
-    rotorEncode(rotorOne, alphabet, 
-    rotorEncode(rotorTwo, alphabet, 
-    rotorEncode(rotorThree, alphabet, 
-    rotorEncode(alphabet, reflector, 
-    rotorEncode(alphabet, rotorThree, 
-    rotorEncode(alphabet, rotorTwo, 
-    rotorEncode(alphabet, rotorOne, 
-    rotorEncode(entryWheel, alphabet, key)))))))));
+  rotorEncode(rotorOne, alphabet, 
+  rotorEncode(rotorTwo, alphabet, 
+  rotorEncode(rotorThree, alphabet, 
+  rotorEncode(alphabet, reflector, 
+  rotorEncode(alphabet, rotorThree, 
+  rotorEncode(alphabet, rotorTwo, 
+  rotorEncode(alphabet, rotorOne, 
+  rotorEncode(entryWheel, alphabet, key)))))))));
 }
 
 char incrementPlace(char place){
   if(place == 'Z'){
     return 'A';
   }
-  return place++;
+  return place + 1;
 }
 
+
+
 void incrementRotors(){
-  if(rotorOnePlace = turnoverOne){
+  if(rotorOnePlace == turnoverOne ){
     turnRotor(rotorTwo);
-    incrementPlace(rotorTwoPlace);
+    rotorTwoPlace = incrementPlace(rotorTwoPlace);
   }
   turnRotor(rotorOne);
-  incrementPlace(rotorOnePlace);
+  rotorOnePlace = incrementPlace(rotorOnePlace);
 
-  if(rotorTwoPlace = turnoverTwo){
-      turnRotor(rotorTwo);
-      incrementPlace(rotorTwoPlace);
-      turnRotor(rotorThree);
-      incrementPlace(rotorThreePlace);
+  if(stepThirdNextTurn){
+    turnRotor(rotorTwo);
+    rotorTwoPlace = incrementPlace(rotorTwoPlace);
+    turnRotor(rotorThree);
+    rotorThreePlace = incrementPlace(rotorThreePlace);
   }
+  stepThirdNextTurn = rotorTwoPlace == turnoverTwo;
 }
 
 void turnRotor(char rotor[26]){ //counter-clockwise
@@ -225,3 +246,7 @@ void ringTurnRotor(char rotor[26]){ //clockwise
   }
   rotor[0] = last;
 }
+
+
+
+
